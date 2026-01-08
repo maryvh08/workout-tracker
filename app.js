4a75842c937808a3c4a8f7ea6b0133aa7c1febdb
MaryVH08 <marycarmen.vives@aneiap.co>
2026-01-08 18:54:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const SUPABASE_URL = "https://vhwfenefevzzksxrslkx.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2ZlbmVmZXZ6emtzeHJzbGt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MTE3ODAsImV4cCI6MjA4MzQ4Nzc4MH0.CG1KzxpxGHifXsgBvH-4E4WvXbj6d-8WsagqaHAtVwo";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
