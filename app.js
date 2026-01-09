9f6656b76849e5f94ff13f2cb898ffa0f0991c65
MaryVH08 <marycarmen.vives@aneiap.co>
2026-01-09 00:37:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let activeMesocycle = null;
 const daySelect = document.getElementById("day-select");
 const exerciseSelect = document.getElementById("exercise-select");
 const exerciseConfig = document.getElementById("exercise-config");
+console.log("exerciseSelect:", exerciseSelect);
 const SUPABASE_URL = "https://vhwfenefevzzksxrslkx.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZod2ZlbmVmZXZ6emtzeHJzbGt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MTE3ODAsImV4cCI6MjA4MzQ4Nzc4MH0.CG1KzxpxGHifXsgBvH-4E4WvXbj6d-8WsagqaHAtVwo";
