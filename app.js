8d9c9c41d75298b337bf31bf8194a85a5491757f
MaryVH08 <marycarmen.vives@aneiap.co>
2026-01-09 02:31:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+console.log("mesocycleList:", mesocycleList);
+
 /* ======================
    SUPABASE
 ====================== */
